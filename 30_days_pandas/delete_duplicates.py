db8cb99e692ca456b96842ef043993de517f3086
UtkarshS20 <Utkarsh2001saxena@gmail.com>
2025-07-08 12:27:48 +0530

    delete duplicate

---
+++ 30_days_pandas/delete_duplicates.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by='id',ascending=True,inplace=True)
+    person = person.drop_duplicates(subset="email", keep="first")
+    return person
+    
+
+person = pd.DataFrame([[1, "john@example.com"],
+[2, "bob@example.com"],
+[3, "john@example.com"]], columns=["id", "email"])
+
+print(delete_duplicate_emails(person))
